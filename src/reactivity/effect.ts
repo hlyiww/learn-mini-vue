dbbf1802102a7e00fa1560c9c02d5bf6b56a1f82
yiwwhl <yiwwhl@qq.com>
2022-11-02 21:58:02 +0800

    feat: sourcecode update effect

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,46 @@
+class ReactiveEffect {
+  private fn;
+  constructor(_fn) {
+    this.fn = _fn;
+  }
+  run() {
+    activeEffect = this;
+    this.fn();
+  }
+}
+
+let activeEffect;
+
+const targetMap = new Map();
+
+export function track(target, key) {
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  if (activeEffect) {
+    dep.add(activeEffect);
+  }
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target);
+  let dep = depsMap.get(key);
+
+  dep.forEach((effect: any) => {
+    effect.run();
+  });
+}
+
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
